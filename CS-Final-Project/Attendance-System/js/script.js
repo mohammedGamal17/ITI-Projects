c8d83b41df1352532b796412cd8dfcdcdefed9bd
Mohammed Gamal <mohammed_gamal7@outlook.com>
2023-04-26 11:31:04 +0200

    End Form Validation

--- CS-Final-Project/Attendance-System/js/script.js
+++ CS-Final-Project/Attendance-System/js/script.js
@@ -1,32 +1,32 @@
-window.addEventListener("load", function () {
-  // preventInspectElement();
-}); // window loaded
+// window.addEventListener("load", function () {
+//   // preventInspectElement();
+// }); // window loaded
 
-/*
- * Prevent Inspect Element *
- */
-function preventInspectElement() {
-  document.addEventListener("contextmenu", (event) => event.preventDefault());
-  document.onkeydown = function (e) {
-    if (e.keyCode == 123) {
-      e.preventDefault();
-      return false;
-    }
-    if (e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0)) {
-      e.preventDefault();
-      return false;
-    }
-    if (e.ctrlKey && e.shiftKey && e.keyCode == "C".charCodeAt(0)) {
-      e.preventDefault();
-      return false;
-    }
-    if (e.ctrlKey && e.shiftKey && e.keyCode == "J".charCodeAt(0)) {
-      e.preventDefault();
-      return false;
-    }
-    if (e.ctrlKey && e.keyCode == "U".charCodeAt(0)) {
-      e.preventDefault();
-      return false;
-    }
-  };
-}
+// /*
+//  * Prevent Inspect Element *
+//  */
+// function preventInspectElement() {
+//   document.addEventListener("contextmenu", (event) => event.preventDefault());
+//   document.onkeydown = function (e) {
+//     if (e.keyCode == 123) {
+//       e.preventDefault();
+//       return false;
+//     }
+//     if (e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0)) {
+//       e.preventDefault();
+//       return false;
+//     }
+//     if (e.ctrlKey && e.shiftKey && e.keyCode == "C".charCodeAt(0)) {
+//       e.preventDefault();
+//       return false;
+//     }
+//     if (e.ctrlKey && e.shiftKey && e.keyCode == "J".charCodeAt(0)) {
+//       e.preventDefault();
+//       return false;
+//     }
+//     if (e.ctrlKey && e.keyCode == "U".charCodeAt(0)) {
+//       e.preventDefault();
+//       return false;
+//     }
+//   };
+// }
